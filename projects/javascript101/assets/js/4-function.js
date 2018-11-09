be134b1195fba9af90f5471bb54034595b2471ef
Alexander Lindholm (DTK) <Alexander.DTK.Lindholm@lystkom.se>
2018-11-09 11:15:44 +0100

    Completed function.js and started array.js

--- projects/javascript101/assets/js/4-function.js
+++ projects/javascript101/assets/js/4-function.js
@@ -1,22 +1,3 @@
-// alert("mumma gay");
-/*
-var name = "Adam";
-var age = 69;
-
-document.write(name);
-
-if(age < 18) {
-    // adams kod, är den sann?
-    console.log("nejnej adam för liten, för han är " + age + " år");
-}
-else if(age == 69) {
-    console.log("adam är dank för han är " + age + " år")
-}
-else {
-    // hoppsan vad hände här
-    console.log("ojoj adam är nog stor, för han är " + age + " år");
-}*/
-
 /*function isEven(x) {
     if(x % 2 == 0) {
         return true
